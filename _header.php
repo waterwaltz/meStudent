847c8ced6ec7888042a5e22eabffe9eb23be2af4
Chloe Chen <waterwaltz@gmail.com>
2017-08-24 22:09:10 +0800

    Add files via upload
    fix firefox bug

---
+++ _header.php
@@ -0,0 +1,5 @@
+<header>
+	<div class="logo">
+		<img src="theme/tw/images/logo.png" alt="Job Calculator">
+	</div>
+</header>
---
+++ index.php
@@ -0,0 +1,405 @@
+<!DOCTYPE html>
+<html>
+<head>	
+	
+<!--for ie-->
+<meta http-equiv="X-UA-Compatible" content="IE=EDGE; IE=10; IE=9; IE=8;" />
+<meta charset="UTF-8">
+
+<title>Job Calculator</title>
+<meta name="viewport" content="width=device-width,initial-scale=1.0">
+<link rel="shortcut icon" href="favicon.ico" type="image/x-icon" />
+
+<!--meta info-->
+<meta name="author" content="">
+<meta name="keywords" content="" />
+<meta name="description" content=""/>
+<meta property="og:title" content="" />
+<meta property="og:description" content="" />
+<meta property="og:site_name" content="">
+<meta property="og:type" content="website">
+<meta property="og:locale" content="en">
+<meta property="og:url" content="">
+<meta property="og:image" content="">
+<meta property="og:image:type" content="image/jpeg">
+<meta property="og:image:width" content="1200">
+<meta property="og:image:height" content="627">
+
+<link rel="stylesheet" href="theme/tw/css/reset.css">
+<link rel="stylesheet" href="theme/tw/css/css.css">
+<link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700" rel="stylesheet">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
+<script type="text/javascript" src="js/jquery-1.8.3.min.js"></script>
+<script type="text/javascript" src="js/script.js"></script>
+<script type='text/javascript' src='js/jquery-imgLiquid.min.js'></script>
+<script type='text/javascript' src="js/jquery.placeholder.js"></script>
+
+<script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+<link rel="stylesheet" href="https://code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
+
+<!-- scrollreveal -->
+<script src="js/scrollreveal.js" type="text/javascript"></script>
+
+
+<!--[if IE]>
+    <script>
+        document.createElement("header");
+        document.createElement("menu");
+        document.createElement("nav");
+        document.createElement("section");
+        document.createElement("article");
+        document.createElement("aside");
+        document.createElement("figure");
+        document.createElement("hgroup");
+        document.createElement("footer");
+        document.createElement("address");
+    </script>
+    <![endif]-->
+    <!--[if lt IE 9]>
+    <script src="js/html5.js"></script>
+<![endif]-->
+
+
+
+<script type="text/javascript">
+$(function(){
+
+    /*placeholder*/
+    $('input, textarea').placeholder();
+
+    // 自動裁圖
+    $('.imgLiquid').imgLiquid({
+        fill: true, horizontalAlign: "center", verticalAlign: "center"
+    });
+
+
+    var spinner = $( ".time_tb input.spinner" ).spinner();
+
+		
+});
+</script>
+
+</head>
+
+<body class="index">
+<div class="wrap">
+
+	<div class="kv">
+		<header>
+			<div class="logo">
+				<img src="theme/tw/images/logo.png" alt="Job Calculator">
+			</div>
+		</header>
+
+			<h1 class="txt">
+				Tell us when you start and finish your job<br>
+				we tell you how much you make
+			</h1>
+
+		<div class="bg imgLiquid">
+			<img src="theme/tw/images/kv.png">
+		</div>
+		<div class="grid"></div>
+	</div>
+
+	<div class="main">
+		<div class="content">
+			<div class="myform fixed">
+				<form action="">
+					<h2 class="form_hd">
+						Calculate your weekly salary
+					</h2>
+					
+					<div class="form_bd">
+						<div class="con">
+							<dl>
+								<dt><h3 class="tit">weekly working hours</h3></dt>
+								<dd class="select_hour">
+									<label for="hour1">
+										<input type="radio" name="hour" id="hour1">
+										<span>20hours / School year</span>
+									</label>
+
+									<label for="hour2">
+										<input type="radio" name="hour" id="hour2">
+										<span>40hours / Holiday</span>
+									</label>
+								</dd>
+							</dl>
+							
+							<dl>
+								<dt><h3 class="tit">your job</h3></dt>
+								<dd>
+									<div class="select1" id="select_job" onchange="show_tb();">
+										<select name="" id="">
+											<option value="">Choose ...</option>
+											<option value="Administrative Assistant">Administrative assistant</option>
+											<option value="Barista">Barista</option>
+											<option value="Bookkeeper">Bookkeeper</option>
+											<option value="Child Care">Child care</option>
+											<option value="Finance Accountant">Finance accountant</option>
+											<option value="Gardener">Gardener</option>
+											<option value="Office Cleaner">Office cleaner</option>
+											<option value="Receptionist">Receptionist</option>
+											<option value="Travel Agency Agent">Travel agency agent</option>
+											<option value="Waiter">Waiter</option>
+										</select>
+									</div>
+								</dd>
+							</dl>
+						</div>
+
+						<div class="con">
+							<dl>
+								<dt><h3 class="tit rate_tit">hourly rate ( $ADU )</h3></dt>
+								<dd>
+									<div class="rate_tb">
+										<table>
+											<tr>
+												<th class="shift">Shifts</th>
+												<td class="shift">06 AM - 09 PM</td>
+												<td class="shift">09 AM - 12 PM</td>
+												<td class="shift">12 AM - 06 PM</td>
+											</tr>
+											<tr>
+												<th>Mon-Fri</th>
+												<td><span>06 AM - 09 PM</span>$18.50 / hr</td>
+												<td><span>06 AM - 09 PM</span>$27.75 / hr</td>
+												<td><span>06 AM - 09 PM</span>$37.00 / hr</td>
+											</tr>
+											<tr>
+												<th>Sat</th>
+												<td><span>09 AM - 12 PM</span>$20.50 / hr</td>
+												<td><span>09 AM - 12 PM</span>$27.75 / hr</td>
+												<td><span>09 AM - 12 PM</span>$37.00 / hr</td>
+											</tr>
+											<tr>
+												<th>Sun</th>
+												<td><span>12 AM - 06 PM</span>$23.13 / hr</td>
+												<td><span>12 AM - 06 PM</span>$27.75 / hr</td>
+												<td><span>12 AM - 06 PM</span>$37.00 / hr</td>
+											</tr>
+										</table>
+									</div>
+								</dd>
+							</dl>
+						</div>
+
+						<div class="con">
+							<dl>
+								<dt><h3 class="tit">timesheet</h3></dt>
+								<dd>
+									<div class="time_tb">
+										<table>
+											<tr>
+												<th>Day</th><th>Start</th><th>Finish</th><th>Total</th>
+											</tr>
+											<tr>
+												<td class="day">Mon</td>
+												<td class="start">
+  													<input class="spinner" type="number" value="0" min="0" max="12">
+													<input class="spinner" type="number" value="0" min="0" max="60">
+													<label class="ampm-switch">
+														  <input type="checkbox" id="" name="" value="" checked="">
+														  <div class="toggle-button"></div>
+													</label>
+												</td>
+												<td class="finish">
+													<input class="spinner" type="number" value="0" min="0" max="12">
+													<input class="spinner" type="number" value="0" min="0" max="60">
+													<label class="ampm-switch">
+														  <input type="checkbox" id="" name="" value="" checked="">
+														  <div class="toggle-button"></div>
+													</label>
+												</td>
+												<td class="total">
+													0.0
+												</td>
+											</tr>
+											<tr>
+												<td class="day">Tue</td>
+												<td class="start">
+  													<input class="spinner" type="number" value="0" min="0" max="12">
+													<input class="spinner" type="number" value="0" min="0" max="60">
+													<label class="ampm-switch">
+														  <input type="checkbox" id="" name="" value="" checked="">
+														  <div class="toggle-button"></div>
+													</label>
+												</td>
+												<td class="finish">
+													<input class="spinner" type="number" value="0" min="0" max="12">
+													<input class="spinner" type="number" value="0" min="0" max="60">
+													<label class="ampm-switch">
+														  <input type="checkbox" id="" name="" value="" checked="">
+														  <div class="toggle-button"></div>
+													</label>
+												</td>
+												<td class="total">
+													0.0
+												</td>
+											</tr>
+											<tr>
+												<td class="day">Wed</td>
+												<td class="start">
+  													<input class="spinner" type="number" value="0" min="0" max="12">
+													<input class="spinner" type="number" value="0" min="0" max="60">
+													<label class="ampm-switch">
+														  <input type="checkbox" id="" name="" value="" checked="">
+														  <div class="toggle-button"></div>
+													</label>
+												</td>
+												<td class="finish">
+													<input class="spinner" type="number" value="0" min="0" max="12">
+													<input class="spinner" type="number" value="0" min="0" max="60">
+													<label class="ampm-switch">
+														  <input type="checkbox" id="" name="" value="" checked="">
+														  <div class="toggle-button"></div>
+													</label>
+												</td>
+												<td class="total">
+													0.0
+												</td>
+											</tr>
+											<tr>
+												<td class="day">Thu</td>
+												<td class="start">
+  													<input class="spinner" type="number" value="0" min="0" max="12">
+													<input class="spinner" type="number" value="0" min="0" max="60">
+													<label class="ampm-switch">
+														  <input type="checkbox" id="" name="" value="" checked="">
+														  <div class="toggle-button"></div>
+													</label>
+												</td>
+												<td class="finish">
+													<input class="spinner" type="number" value="0" min="0" max="12">
+													<input class="spinner" type="number" value="0" min="0" max="60">
+													<label class="ampm-switch">
+														  <input type="checkbox" id="" name="" value="" checked="">
+														  <div class="toggle-button"></div>
+													</label>
+												</td>
+												<td class="total">
+													0.0
+												</td>
+											</tr>
+											<tr>
+												<td class="day">Fri</td>
+												<td class="start">
+  													<input class="spinner" type="number" value="0" min="0" max="12">
+													<input class="spinner" type="number" value="0" min="0" max="60">
+													<label class="ampm-switch">
+														  <input type="checkbox" id="" name="" value="" checked="">
+														  <div class="toggle-button"></div>
+													</label>
+												</td>
+												<td class="finish">
+													<input class="spinner" type="number" value="0" min="0" max="12">
+													<input class="spinner" type="number" value="0" min="0" max="60">
+													<label class="ampm-switch">
+														  <input type="checkbox" id="" name="" value="" checked="">
+														  <div class="toggle-button"></div>
+													</label>
+												</td>
+												<td class="total">
+													0.0
+												</td>
+											</tr>
+											<tr>
+												<td class="day">Sat</td>
+												<td class="start">
+  													<input class="spinner" type="number" value="0" min="0" max="12">
+													<input class="spinner" type="number" value="0" min="0" max="60">
+													<label class="ampm-switch">
+														  <input type="checkbox" id="" name="" value="" checked="">
+														  <div class="toggle-button"></div>
+													</label>
+												</td>
+												<td class="finish">
+													<input class="spinner" type="number" value="0" min="0" max="12">
+													<input class="spinner" type="number" value="0" min="0" max="60">
+													<label class="ampm-switch">
+														  <input type="checkbox" id="" name="" value="" checked="">
+														  <div class="toggle-button"></div>
+													</label>
+												</td>
+												<td class="total">
+													0.0
+												</td>
+											</tr>
+											<tr>
+												<td class="day">Sun</td>
+												<td class="start">
+  													<input class="spinner" type="number" value="0" min="0" max="12">
+													<input class="spinner" type="number" value="0" min="0" max="60">
+													<label class="ampm-switch">
+														  <input type="checkbox" id="" name="" value="" checked="">
+														  <div class="toggle-button"></div>
+													</label>
+												</td>
+												<td class="finish">
+													<input class="spinner" type="number" value="0" min="0" max="12">
+													<input class="spinner" type="number" value="0" min="0" max="60">
+													<label class="ampm-switch">
+														  <input type="checkbox" id="" name="" value="" checked="">
+														  <div class="toggle-button"></div>
+													</label>
+												</td>
+												<td class="total">
+													0.0
+												</td>
+											</tr>
+										</table>
+										<div class="total_num clearfix">
+											<div class="right">
+												<b class="subtotal">Subtotal</b>
+												<span>16.0</span>
+											</div>
+											<span class="warning">
+												<i class="fa fa-warning"></i> You can't exceed the maximum number of hours
+											</span>
+										</div>
+
+										<ul class="tb_btn clearfix">
+											<li class="orange">
+												<a href="">Calculate</a>
+											</li>
+											<li class="gray">
+												<a href="">Reset</a>
+											</li>
+										</ul>
+									</div>
+								</dd>
+							</dl>
+						</div>
+					</div>
+						<div class="myresult clearfix fixed">
+							<div class="hour">Weekly hours: 16/40</div>
+							<div class="salary">
+								<span>
+									salary<b>$296.00</b>
+								</span>
+								
+								<div class="currency">
+									<div class="select1">
+										<select name="" id="">
+											<option value="">AUD</option>
+											<option value="">EUR</option>
+											<option value="">NTD</option>
+											<option value="">USD</option>
+											<option value="">RMB</option>
+										</select>
+									</div>
+								</div>
+							</div>
+						</div>
+				</form>
+			</div>
+		</div>
+	</div>
+</div>
+
+<footer>
+</footer>
+
+</body>
+</html>
