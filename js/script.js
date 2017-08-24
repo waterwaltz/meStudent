28ba85cf9b64f6292b29813f89b33e4f6af993ea
Chloe Chen <waterwaltz@gmail.com>
2017-08-24 22:26:37 +0800

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -4,9 +4,7 @@ function show_tb(){
 }
 
 
-
 $(function(){
-
 
 	$('.rate_tit').click(function(){
 		if( $(this).hasClass('actived')){
@@ -21,6 +19,18 @@ $(function(){
 
 
 
+	var thisdevice = navigator.userAgent;
+	if (thisdevice.match(/(iPhone|iPod|iPad|Android)/)){
+		
+		$('.myresult').hide();
+		$(window).scroll(function() {
+		   if($(window).scrollTop()  > 300) {
+		       $('.myresult').fadeIn();
+		   }else if($(window).scrollTop()  > 100){
+		   		$('.myresult').fadeOut();
+		   }
+		});	
+	}
 
 
 });
